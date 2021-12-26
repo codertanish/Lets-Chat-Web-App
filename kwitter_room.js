4015578155028813e174720baf43a5de72aab830
codertanish <81834790+codertanish@users.noreply.github.com>
2021-12-25 21:30:35 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCORjPR2DMMFLCRcdiXHrKYzjnKR6NOveE",
+    authDomain: "lets-chat-web-app-5337e.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-5337e-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-5337e",
+    storageBucket: "lets-chat-web-app-5337e.appspot.com",
+    messagingSenderId: "483559033336",
+    appId: "1:483559033336:web:05a8c03f25924335daee0d"
+  };
+  
+ firebase.initializeApp(firebaseConfig);
